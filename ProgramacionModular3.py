faab50b217e6256619c001193791552642c0ebba
josevazquez99 <jv319069@gmail.com>
2022-11-25 10:03:19 +0100

    ejercicios

--- ProgramacionModular3.py
+++ ProgramacionModular3.py
@@ -4,75 +4,122 @@ Created on Nov 23, 2022
 @author: estudiante
 '''
 #Ejercicio 1
-caracter="".upper()
-cadena="Hola Maria".upper()
-def characterInString(caracter):
-    contador=0
-    for i in cadena:
-        if caracter==i:
-            contador+=1
-    return contador
-print(characterInString("A"))
+# caracter="".upper()
+# cadena="Hola Maria".upper()
+# def characterInString(caracter):
+#     contador=0
+#     for i in cadena:
+#         if caracter==i:
+#             contador+=1
+#     return contador
+# print(characterInString("A"))
 
 #Ejercicio 2
-cadena="Hola Maria"
-def lowCaseInString ():
-    contador=0
-    for i in cadena:
-        if i==i.lower() and i!=" ":
-            contador+=1
-    return contador
-print(lowCaseInString())
+# cadena="Hola Maria"
+# def lowCaseInString ():
+#     contador=0
+#     for i in cadena:
+#         if i==i.lower() and i!=" ":
+#             contador+=1
+#     return contador
+# print(lowCaseInString())
 
 #Ejercicio 3
 
-cadena="Hola Maria"
-def upperCaseInString ():
-    contador=0
-    for i in cadena:
-        if i==i.upper() and i!=" ":
-            contador+=1
-    return contador
-print(upperCaseInString())
+# cadena="Hola Maria"
+# def upperCaseInString ():
+#     contador=0
+#     for i in cadena:
+#         if i==i.upper() and i!=" ":
+#             contador+=1
+#     return contador
+# print(upperCaseInString())
 
 #Ejercicio 4
-cadena="Hola Maria 1970"
-def numberInString():
-    numeros=str([0,1,2,3,4,5,6,7,8,9])
-    contador=0
-    for i in cadena:
-        if i in numeros and i!=" ":
-            contador+=1
-    return contador
-print(numberInString())
+# cadena="Hola Maria 1970"
+# def numberInString():
+#     numeros=str([0,1,2,3,4,5,6,7,8,9])
+#     contador=0
+#     for i in cadena:
+#         if i in numeros and i!=" ":
+#             contador+=1
+#     return contador
+# print(numberInString())
 
 #Ejercicio 5
-cadena="anilina"
-def palindrome():
-    cierto=False
-    if cadena==cadena[::-1]:
-        cierto=True
-    return cierto
-print(palindrome())
+# cadena="anilina"
+# def palindrome():
+#     cierto=False
+#     if cadena==cadena[::-1]:
+#         cierto=True
+#     return cierto
+# print(palindrome())
 
 #Ejercicio 6
-cadena="shybaoxlna"
-palabra="hola"
-def buscarPalabraEscondida():
-    encontrado=False
-    for i in cadena:
-        for j in palabra:
-            if i==j:
-                encontrado=True
-    return encontrado
-print(buscarPalabraEscondida())
+# cadena="shybaoxlna"
+# palabra="hola"
+# def buscarPalabraEscondida():
+#     encontrado=False
+#     for i in cadena:
+#         for j in palabra:
+#             if i==j:
+#                 encontrado=True
+#     return encontrado
+# print(buscarPalabraEscondida())
 
 #Ejercicio 7
-cadena="Mo moto ol pono doroponto"
-letraBuscar="o"
-letraCambiar="e"
+def buscarReemplazarPalabra(cadena,cadenaBuscar,cadenaRemplazar):
+    for i in range(len(cadena)+1):
+        for j in range(len(cadena)+1):
+            if cadena[i]==cadenaBuscar[j]:
+                cadenaBuscar==cadenaRemplazar
+    return cadena
+print(buscarReemplazarPalabra("HOLA MUNDO","HOLA","ADIOS"))
 
-cadena.replace(letraBuscar, letraCambiar)        
+#Ejercicio 8 
+# vocales=['a','e','i','o','u']
+# palabra=""
+# listaSinRepetir=[]
+# def obtenerVocales(palabra):
+#     contador=0
+#     for i in palabra:
+#         if i.lower() in "aeiou":
+#             listaSinRepetir.append(i)
+#     for j in listaSinRepetir:
+#         if j!=i:
+#             contador+=1
+#
+#     return contador
+# print(obtenerVocales("Abaco"))
+
+#Ejercicio 9
+#
+# def ponerUltimasVocales(cadena):
+#     separadorConsonante=""
+#     separadorVocal=""
+#     for i in cadena:
+#         if i.lower() in "aeiou":
+#             separadorVocal+=i
+#         elif i==" ":
+#             separadorVocal+="" 
+#         else:
+#             separadorConsonante+=i
+#
+#     return separadorConsonante+separadorVocal
+# print(ponerUltimasVocales("curso de programacion"))
+
+#Ejercicio 10
+# def obtenerNumeroPalabras(cadena):
+#     contador=0
+#     if cadena[0]!=" ":
+#         contador+=1
+#     for i in range(len(cadena)-1):
+#         if cadena[i]==" " and cadena[i+1] !=" ":
+#             contador+=1
+#     return contador
+#
+# print(obtenerNumeroPalabras(" He estudiado mucho "))
+        
 
 
 
